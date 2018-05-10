e5dc2ad67228990b48058ae046dd4a00d0c79c19
andrewShelton <andrew.shelton@slalom.com>
2018-05-10 15:18:32 -0500

    successfully added in angularfire2 modules

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA2u8zLv4_UvAwYhAW4tMe8Ovws5TgPVso',
+    authDomain: 'aks-udemy-shop.firebaseapp.com',
+    databaseURL: 'https://aks-udemy-shop.firebaseio.com',
+    projectId: 'aks-udemy-shop',
+    storageBucket: 'aks-udemy-shop.appspot.com',
+    messagingSenderId: '432022531922'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA2u8zLv4_UvAwYhAW4tMe8Ovws5TgPVso',
+    authDomain: 'aks-udemy-shop.firebaseapp.com',
+    databaseURL: 'https://aks-udemy-shop.firebaseio.com',
+    projectId: 'aks-udemy-shop',
+    storageBucket: 'aks-udemy-shop.appspot.com',
+    messagingSenderId: '432022531922'
+  }
 };
 
 /*
